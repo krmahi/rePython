9b1a869f9dc0cd8c0bbe5d58464690f4d8ef5b61
krmahi <mahesh.kr.2277@gmail.com>
2025-03-24 17:35:39 +0530

    +import form graph_vertex

--- UndirectedGraph(adjacency_list).py
+++ UndirectedGraph(adjacency_list).py
@@ -1,10 +1,4 @@
-class vertex:
-    def __init__(self, n):
-        self.name = n
-        self.neighbors = set()
-
-    def add_neighbor(self, v):
-        self.neighbors.add(v)
+from Graph_vertex import vertex
 
 class graph:
     vertices = {}
