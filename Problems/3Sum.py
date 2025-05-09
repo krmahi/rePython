e45f8cd1b77fd405a1c13ccc45ad0c5113f450bd
krmahi <mahesh.kr.2277@gmail.com>
2025-05-09 21:11:13 +0530

    update: 3sum: Two pointers -> O(n^2)

--- Problems/3Sum.py
+++ Problems/3Sum.py
@@ -13,3 +13,31 @@ class Solution:
                         res.add(tuple(tmp))
 
         return [list(i) for i in res]
+
+# Two pointers: O(n^2)
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        # res = set()
+        n = len(nums)
+
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            left, right = i + 1, n - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0: left += 1
+                elif total < 0: right -= 1
+                else:
+                    res.append([nums[i] + nums[left] + nums[right]])
+                    # res.add(tuple([nums[i] + nums[left] + nums[right]]))
+                    while left < right and nums[left] == nums[left + 1]: left += 1
+                    while left < right and nums[right] == nums[right - 1]: right -= 1
+                    left += 1
+                    right -= 1
+
+        return res
+        # return [list(i) for i in res]
