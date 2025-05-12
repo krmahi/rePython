6109286abc380f434370306638f2e8158dc3a322
krmahi <mahesh.kr.2277@gmail.com>
2025-05-12 20:41:19 +0530

    add: Design Twitter (Optimal) : time -> O(k) + O(10*logk) + O(1) => O(k) + O(1)

--- Problems/DesignTwitter.py
+++ Problems/DesignTwitter.py
@@ -54,3 +54,42 @@ class Twitter:
     def Unfollow(self, followerId, followeeId):
         self.followMap[followerId].discard(followeeId) # discard dosent throw any error if item not in set 
 
+# heap optimised -> time: O(1) + O(k) + O(10.logk) => O(1) + O(k)
+class Twitter:
+    def __init__(self):
+        self.time = 0
+        self.tweetMap = defaultdict(list)
+        self.followMap = defaultdict(set)
+
+    def postTweet(self, UserId, TweetId):
+        self.tweetMap[UserId].append([self.time, TweetId])
+        self.time -= 1
+
+    def getFeed(self, UserId):
+        # time -> O(10*logk) + O(k) => O(k) || k -> no. of followeeId 
+        res = []
+        minHeap = []
+
+        self.follow(UserId, UserId)
+        for followeeId in self.followMap[UserId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                time, tweetId = self.tweetMap[followeeId][index]
+                minHeap.append([time, tweetId, followeeId, index - 1])
+        
+        heapq.heapify(minHeap)
+        while minHeap and len(res) < 10:
+            time, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            
+            if index <= 0:
+                time, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush([time, tweetId, followeeId, index - 1])
+
+        return res 
+    
+    def follow(self, followerId, followeeId):
+        self.followMap[followerId].add(followeeId)
+
+    def Unfollow(self, followerId, followeeId):
+        self.followMap[followerId].discard(followeeId) # discard dosent throw any error if item not in set 
